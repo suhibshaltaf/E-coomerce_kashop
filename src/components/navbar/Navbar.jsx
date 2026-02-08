31b553324260f5ef20110530abaec08b236d0c14
sohaibshaltaf <sohaibshaltafaltrefe@gmail.com>
2026-02-08 19:13:03 +0300

    create navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link as RouterLink } from 'react-router-dom';
+import { Badge, Icon, Link } from '@mui/material';
 
 export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+   return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+        
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            KASHOP
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'flex' }, gap: 2 ,alignItems: 'center' }}  >
+          <Link  component={RouterLink} to="/" color="inherit" underline='none'>Home</Link>
+          <Link component={RouterLink} to="/login" color="inherit" underline='none'>Login</Link>
+          <Link component={RouterLink} to="/register" color="inherit" underline='none'>Register</Link>
+            <IconButton color="inherit"  component={RouterLink} to="/cart">
+      <Badge  color="secondary">
+        <ShoppingCartIcon />
+      </Badge>
+    </IconButton>
+            </Box>
+            <IconButton
+          
+              sx={{ display: { xs: 'flex', sm: 'none' } }}
+              color="inherit"
+            >
+
+              <MenuIcon  />
+            </IconButton >        
+        </Toolbar> 
+
+      </AppBar>
+    </Box>
+  );
 }
