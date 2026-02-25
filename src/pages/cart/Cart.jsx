6534b6ff1d2808f7cce5aca4607b86c91d6d6a88
sohaibshaltaf <sohaibshaltafaltrefe@gmail.com>
2026-02-25 15:44:21 +0300

    remove context

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,13 +1,11 @@
-import React, { useContext } from 'react'
 import useCart from '../../hooks/useCart';
-import { UserContext } from '../../context/Usercontext';
 
 export default function Cart() {
   const {data,isLoading,isError} = useCart();
-  const username=useContext(UserContext);
 
   console.log(data);
   return (
-    <div>Cart - {username}</div>
+    <div>Cart - </div>
   )
 }
+  
--- src/router.jsx
+++ src/router.jsx
@@ -4,14 +4,13 @@ import Home from "./pages/home/Home";
 import Cart from "./pages/cart/Cart";
 import Login from "./pages/auth/login/Login";
 import Register from "./pages/auth/register/Register";
-import UserContextProvider from "./context/Usercontext";
 
 const router= createBrowserRouter([
 
     {
         path:'/',
-        element:<UserContextProvider>
-                    <MainLayout/> </UserContextProvider>,
+        element:
+                    <MainLayout/> ,
         children:[
             {
                 index:true,
